3758fc460d9e38c0163c76936ec0b15cc88a2ef8
Rafi <masitingss@gmail.com>
2019-09-01 05:20:51 +0700

    Execption

--- init.php
+++ init.php
@@ -1,6 +1,15 @@
 <?php 
+	
+
 	function login_with_creds($username, $password)
 	{
-		$ig = \InstagramScraper\Instagram::withCredentials($username, $password, dirname(__FILE__).'/tmp/cache/');
-		return $ig->login(true);
+		$website = str_replace(['http://', 'https://', '/'], '', site_url());
+		$cache_path = dirname(__FILE__).'/tmp/cache/'.$website.'/';
+
+		$ig = \InstagramScraper\Instagram::withCredentials($username, $password, $cache_path);
+		if (!file_exists($cache_path)) {
+			return $ig->login(true);
+		}
+		$account = $ig->getAccountById(1000);
+		return $account['profilePicUrl'];
 	}
