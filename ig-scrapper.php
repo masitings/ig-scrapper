15df45108a4c530a961c54726e9bcd9b4f6a8610
Rafi Bagaskara Halilintar <masitingss@gmail.com>
2019-09-02 09:48:09 +0700

    Add updater

--- ig-scrapper.php
+++ ig-scrapper.php
@@ -3,11 +3,21 @@
 Plugin Name: Instagram Scrapper (Developer Version)
 Plugin URI: https://semanthic.com/
 Description: Plugin for scrape feed from instagram.
-Version: 1.0.0
+Version: 1.0.1
 Author: Semanthic
 Author URI: https://semanthic.com/
 License: Semanthic.
 */
+
+//Updater
+require 'updater/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/masitings/ig-scrapper/',
+	__FILE__,
+	'ig-scrapper'
+);
+$myUpdateChecker->setBranch('master');
+
 include dirname(__FILE__) . '/vendor/autoload.php';
 include dirname(__FILE__) . '/controller/IG_Scrapper.php';
 include dirname(__FILE__) . '/init.php';
